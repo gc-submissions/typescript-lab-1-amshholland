cf5b9985133536bf55eab7ab4c11fa42efa7efa1
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-04 09:17:19 -0400

    reverted inventory

--- src/inventory.ts
+++ src/inventory.ts
@@ -1,13 +1,14 @@
 import { Product } from "../src/products";
 
-interface InventoryItem extends Product {
+interface InventoryItem {
+    product: Product;
     quantity: number;
 }
 
-const inventory: InventoryItem[] = [
-    { name: "motor", price: 10.00, quantity: 10 },
-    { name: "sensor", price: 12.50, quantity: 4 },
-    { name: "LED", price: 1.00, quantity: 20 }
+let inventory: InventoryItem[] = [
+    { product: { name: "motor", price: 10.00 }, quantity: 10 },
+    { product: { name: "sensor", price: 12.50 }, quantity: 4 },
+    { product: { name: "LED", price: 1.00 }, quantity: 20 }
 ];
 
 export function calcInventoryValue(inventory: InventoryItem[]): number {
@@ -15,7 +16,7 @@ export function calcInventoryValue(inventory: InventoryItem[]): number {
 
     if (inventory.length !== 0) {
         inventory.forEach(function (item) {
-            sum += inventory.price * inventory.quantity;
+            sum += item.price * item.quantity;
         })
         return sum;
     }
